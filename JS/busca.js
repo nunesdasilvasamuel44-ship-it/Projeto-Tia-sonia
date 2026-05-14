3b3e4f4e6e4dd94eed1b0507c77f6a55b7fbf4dd
Samuel Nunes da Silva <samuel62622766@edu.df.senac.br>
2026-05-14 17:30:47 -0300

    completabusca

--- JS/busca.js
+++ JS/busca.js
@@ -2,3 +2,33 @@ const formBusca = document.querySelector('.busca');
 const campoBusca = document.querySelector('.busca__campo');
 
 const itemCatalagos = document.querySelectorAll('.bordados'); 
+
+//verificar se o formulario de busca existe na pagina
+
+if(formBusca){
+    formBusca.addEventListener('sumbit',  function(event){
+        event.preventDefault();
+
+    } )
+
+}
+
+//Agora vamos verificar o texto que foi digitado 
+
+campoBusca.addEventListener('input', function(){
+    
+    const termoBusca = campoBusca.value.toLowerCase();
+
+    itemCatalagos.forEach(function(item){
+
+        const titulo = item.querySelector('h3').innerText.toLocaleLowerCase()
+
+        if(titulo.includes(termoBusca)){
+            item.style.display = 'block';
+            
+        }else{
+           item.style.display = 'none';
+
+        }
+    })
+})
